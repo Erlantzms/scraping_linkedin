e0df70def1f05377a507a7a28e7e880dc2370abd
Erlantz Maturana <erlantzms@gmail.com>
2023-08-09 12:57:56 +0200

    send data as response

--- index.js
+++ index.js
@@ -5,10 +5,9 @@ const port = process.env.PORT || 3000;
 
 // // Define route for GET request on path '/'
 app.get('/data', async (request, response) => {
-//   const data = await processJobs()
-  console.log("dataaaaaaa")
-  response.send({data:"data"});
+  const data = await processJobs()
+  response.send(data);
 });
 
 // // Start the server on port 3000
-app.listen(port, "0.0.0.0", () => console.log(`Server listening on port 3000.`));
+app.listen(port, "0.0.0.0", () => console.log(`Server listening on port ${port}.`));
