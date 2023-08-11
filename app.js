deb7945beed232bbbf0b7b647c63c6f9389f4d42
Erlantz Maturana <erlantzms@gmail.com>
2023-08-11 10:26:55 +0200

    install cors

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
-
+const cors = require('cors');
 const app = express();
+
+app.use(cors());
 
 app.use((request, response) => {
    response.json({ message: 'Hey! This is your server response!' }); 
